b3458992c277ae563e820f8637dd8001eb15494f
quest1onmark <jonas.raphael.schultheiss@gmail.com>
2019-06-25 02:05:59 +0200

    latest

--- src/components/Chatlist.jsx
+++ src/components/Chatlist.jsx
@@ -4,11 +4,14 @@ import ChatItem from './ChatItem';
 
 export default class Chatlist extends Component {
   render() {
-    
+    let arr = [1, 2, 3, 4, 5];
+
     return (
-      <ListGroup as="ul" style={{paddingTop: 10}}>
-      {arr.map(i => <ChatItem />)}
+      <ListGroup as="ul" style={{ paddingTop: 10 }}>
+        {arr.map(i => (
+          <ChatItem />
+        ))}
       </ListGroup>
-    )
+    );
   }
 }
